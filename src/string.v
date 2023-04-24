6eedb620b725422b538159bcc674cfc400a7e436
Filip Vrba <filipvrba@pm.me>
2023-04-24 10:14:09 +0200

    add sub

--- src/string.v
+++ src/string.v
@@ -1,9 +1,16 @@
 module rb
 
+import regex
+
 pub struct String {
 	self string
 }
 
 pub fn (s String) to_v() string {
 	return s.self
+}
+
+pub fn (s String) sub(pattern string, value string) String {
+	mut re := regex.regex_opt(pattern) or { panic(err) }
+	return String{ re.replace(s.self, value) }
 }
